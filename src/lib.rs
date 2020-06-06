c2e7f39aaf7f2ee764ac9cd12571d62b105202a6
sansx <xiathxth@gmail.com>
2020-06-06 21:51:36 +0800

    :white_check_mark: add test

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ impl Universe {
         let height = 64;
 
         let cells = (0..width * height)
-            .map(|i| {
+            .map(|_i| {
                 if js_sys::Math::random() < 0.5 {
                     Cell::Alive
                 } else {
@@ -50,8 +50,18 @@ impl Universe {
         self.to_string()
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn width(&self) -> u32 {
         self.width
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn height(&self) -> u32 {
@@ -123,5 +133,18 @@ impl fmt::Display for Universe {
         }
 
         Ok(())
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
