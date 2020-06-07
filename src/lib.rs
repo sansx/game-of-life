b7f5f4cb61317772b18047887403a3353a4859c1
sansx <xiathxth@gmail.com>
2020-06-07 13:57:01 +0800

    :new: trigger cell on canvas

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,15 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
@@ -79,6 +88,11 @@ impl Universe {
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
     }
 
     fn get_index(&self, row: u32, column: u32) -> usize {
