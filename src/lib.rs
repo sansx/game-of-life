5206d7b05e08a32c23f64fb5dfd08a920bc80239
sansx <xiathxth@gmail.com>
2020-08-26 17:40:55 +0800

    :sparkles: prioritization render

--- src/lib.rs
+++ src/lib.rs
@@ -6,183 +6,194 @@ extern crate web_sys;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
-    ( $( $t:tt )* ) => {
-        // web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
+  ( $( $t:tt )* ) => {
+    // web_sys::console::log_1(&format!( $( $t )* ).into());
+  };
 }
 
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
-    Dead = 0,
-    Alive = 1,
+  Dead = 0,
+  Alive = 1,
 }
 
 #[wasm_bindgen]
 pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
+  width: u32,
+  height: u32,
+  cells: Vec<Cell>,
 }
 
 impl Cell {
-    fn toggle(&mut self) {
-        *self = match *self {
-            Cell::Dead => Cell::Alive,
-            Cell::Alive => Cell::Dead,
-        };
-    }
+  fn toggle(&mut self) {
+    *self = match *self {
+      Cell::Dead => Cell::Alive,
+      Cell::Alive => Cell::Dead,
+    };
+  }
 }
 
 #[wasm_bindgen]
 impl Universe {
-    pub fn new() -> Universe {
-        utils::set_panic_hook();
-        let width = 64;
-        let height = 64;
+  pub fn new() -> Universe {
+    utils::set_panic_hook();
+    let width = 64;
+    let height = 64;
 
-        let cells = (0..width * height)
-            .map(|_i| {
-                if js_sys::Math::random() < 0.5 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-                // if i % 2 == 0 || i % 7 == 0 {
-                //     Cell::Alive
-                // } else {
-                //     Cell::Dead
-                // }
-            })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells,
+    let cells = (0..width * height)
+      .map(|_i| {
+        if js_sys::Math::random() < 0.5 {
+          Cell::Alive
+        } else {
+          Cell::Dead
         }
-    }
+        // if i % 2 == 0 || i % 7 == 0 {
+        //     Cell::Alive
+        // } else {
+        //     Cell::Dead
+        // }
+      })
+      .collect();
 
-    pub fn render(&self) -> String {
-        self.to_string()
+    Universe {
+      width,
+      height,
+      cells,
     }
+  }
 
-    pub fn set_width(&mut self, width: u32) {
-        self.width = width;
-        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
-    }
+  pub fn render(&self) -> String {
+    self.to_string()
+  }
 
-    pub fn width(&self) -> u32 {
-        self.width
-    }
+  pub fn set_width(&mut self, width: u32) {
+    self.width = width;
+    self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+  }
 
-    pub fn set_height(&mut self, height: u32) {
-        self.height = height;
-        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
-    }
+  pub fn width(&self) -> u32 {
+    self.width
+  }
 
-    pub fn height(&self) -> u32 {
-        self.height
-    }
+  pub fn set_height(&mut self, height: u32) {
+    self.height = height;
+    self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+  }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
+  pub fn height(&self) -> u32 {
+    self.height
+  }
 
-    pub fn toggle_cell(&mut self, row: u32, column: u32) {
-        let idx = self.get_index(row, column);
-        self.cells[idx].toggle();
-    }
+  pub fn cells(&self) -> *const Cell {
+    self.cells.as_ptr()
+  }
 
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
+  pub fn toggle_cell(&mut self, row: u32, column: u32) {
+    let idx = self.get_index(row, column);
+    self.cells[idx].toggle();
+  }
 
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_col == 0 && delta_row == 0 {
-                    continue;
-                }
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
+  fn get_index(&self, row: u32, column: u32) -> usize {
+    (row * self.width + column) as usize
+  }
+
+  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+    for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+      for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+        if delta_col == 0 && delta_row == 0 {
+          continue;
         }
-        count
+        let neighbor_row = (row + delta_row) % self.height;
+        let neighbor_col = (column + delta_col) % self.width;
+        let idx = self.get_index(neighbor_row, neighbor_col);
+        count += self.cells[idx] as u8;
+      }
     }
+    count
+  }
 
-    pub fn destory(&mut self) {
-      // 123
-      self.cells = self.cells.iter().cloned().map(|_i| Cell::Dead).collect();
+  pub fn destory(&mut self) {
+    self.cells = self.cells.iter().map(|_i| Cell::Dead).collect();
+  }
+
+  pub fn reset (&mut self) {
+    self.destory();
+    self.cells = (0..self.width * self.height)
+    .map(|_i| {
+      if js_sys::Math::random() < 0.5 {
+        Cell::Alive
+      } else {
+        Cell::Dead
+      }
+    })
+    .collect()
+  }
+
+  pub fn tick(&mut self) {
+    let mut next = self.cells.clone();
+    for row in 0..self.height {
+      for col in 0..self.width {
+        let idx = self.get_index(row, col);
+        let cell = self.cells[idx];
+        let live_neighbors = self.live_neighbor_count(row, col);
+
+        log!(
+          "cell[{}, {}] is initially {:?} and has {} live neighbors",
+          row,
+          col,
+          cell,
+          live_neighbors
+        );
+
+        let next_cell = match (cell, live_neighbors) {
+          // Rule 1: Any live cell with fewer than two live neighbours
+          // dies, as if caused by underpopulation.
+          (Cell::Alive, x) if x < 2 => Cell::Dead,
+          // Rule 2: Any live cell with two or three live neighbours
+          // lives on to the next generation.
+          (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+          // Rule 3: Any live cell with more than three live
+          // neighbours dies, as if by overpopulation.
+          (Cell::Alive, x) if x > 3 => Cell::Dead,
+          // Rule 4: Any dead cell with exactly three live neighbours
+          // becomes a live cell, as if by reproduction.
+          (Cell::Dead, 3) => Cell::Alive,
+          // All other cells remain in the same state.
+          (otherwise, _) => otherwise,
+        };
+        log!("    it becomes {:?}", next_cell);
+        next[idx] = next_cell;
+      }
     }
-
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-
-                log!(
-                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                    row,
-                    col,
-                    cell,
-                    live_neighbors
-                );
-
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-                log!("    it becomes {:?}", next_cell);
-                next[idx] = next_cell;
-            }
-        }
-        self.cells = next;
-    }
+    self.cells = next;
+  }
 }
 
 impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '□' } else { '■' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-
-        Ok(())
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    for line in self.cells.as_slice().chunks(self.width as usize) {
+      for &cell in line {
+        let symbol = if cell == Cell::Dead { '□' } else { '■' };
+        write!(f, "{}", symbol)?;
+      }
+      write!(f, "\n")?;
     }
+    Ok(())
+  }
 }
 
 impl Universe {
-    pub fn get_cells(&self) -> &[Cell] {
-        &self.cells
-    }
+  pub fn get_cells(&self) -> &[Cell] {
+    &self.cells
+  }
 
-    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
-        for (row, col) in cells.iter().cloned() {
-            let idx = self.get_index(row, col);
-            self.cells[idx] = Cell::Alive;
-        }
+  pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+    for (row, col) in cells.iter().cloned() {
+      let idx = self.get_index(row, col);
+      self.cells[idx] = Cell::Alive;
     }
+  }
 }
--- www/index.js
+++ www/index.js
@@ -12,16 +12,16 @@ import { memory } from "test1/test1_bg";
 
 //     requestAnimationFrame(renderLoop);
 // };
-
-
 // requestAnimationFrame(renderLoop);
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
-let generType;
+// const DEAD_COLOR = "#FFFFFF";
+// const ALIVE_COLOR = "#000000";
+const DEAD_COLOR = '#B6BCC2';
+const ALIVE_COLOR = "#2E3635";
 
+let generType;
 
 const rangeIpt = document.getElementById('rangeipt')
 console.log(rangeIpt.value);
@@ -42,7 +42,6 @@ const ctx = canvas.getContext('2d');
 const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
-
   // Vertical lines.
   for (let i = 0; i <= width; i++) {
     ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
@@ -54,7 +53,6 @@ const drawGrid = () => {
     ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
-
   ctx.stroke();
 };
 
@@ -68,13 +66,48 @@ const drawCells = () => {
 
   ctx.beginPath();
 
+  // for (let row = 0; row < height; row++) {
+  //   for (let col = 0; col < width; col++) {
+  //     const idx = getIndex(row, col);
+
+  //     ctx.fillStyle = cells[idx] === Cell.Dead
+  //       ? DEAD_COLOR
+  //       : ALIVE_COLOR;
+
+  //     ctx.fillRect(
+  //       col * (CELL_SIZE + 1) + 1,
+  //       row * (CELL_SIZE + 1) + 1,
+  //       CELL_SIZE,
+  //       CELL_SIZE
+  //     );
+  //   }
+  // }
+
+  // Alive cells.
+  ctx.fillStyle = ALIVE_COLOR;
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Alive) {
+        continue;
+      }
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
 
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOR
-        : ALIVE_COLOR;
+  // Dead cells.
+  ctx.fillStyle = DEAD_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Dead) {
+        continue;
+      }
 
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
@@ -96,6 +129,7 @@ let animationId = null;
 
 const renderLoop = () => {
   // debugger;
+  fps.render();
   universe.tick();
   drawGrid();
   drawCells();
@@ -115,6 +149,7 @@ const isPaused = () => {
 const playPauseButton = document.getElementById("play-pause");
 const nextButton = document.getElementById('next-step')
 const destoryButton = document.getElementById('boom')
+const resetButton = document.getElementById('reset')
 
 const play = () => {
   playPauseButton.textContent = "||";
@@ -159,6 +194,12 @@ rangeIpt.addEventListener('change', ({ target: { value } }) => {
   renderLoop();
 })
 
+resetButton.addEventListener('click', () => {
+  universe.reset();
+  drawGrid();
+  drawCells();
+})
+
 canvas.addEventListener("click", event => {
   const boundingRect = canvas.getBoundingClientRect();
 
@@ -197,3 +238,48 @@ document.onkeyup = () => {
 }
 
 // renderLoop()
+
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+    `.trim();
+  }
+};
+
+
+
